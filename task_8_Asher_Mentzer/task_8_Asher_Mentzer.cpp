2a7390419fd2dbd1f252dadc46daf3dc06b2f7cc
Asher <ashmenzer@gmail.com>
2023-07-11 02:29:32 +0300

    add all Cmake files

---
+++ task_8_Asher_Mentzer/task_8_Asher_Mentzer.cpp
@@ -0,0 +1,12 @@
+﻿// task_8_Asher_Mentzer.cpp : Defines the entry point for the application.
+//
+
+#include "task_8_Asher_Mentzer.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
